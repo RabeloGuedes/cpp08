3d86a8798fba5fae355ee07cbaacde27800c7b21
rabeloguedes <rabeloguedes@proton.me>
2024-10-24 19:59:37 +0100

    fix: Changed the implementation of easyfind to work with algorithms.

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -5,35 +5,14 @@
 # include <vector>
 # include <list>
 # include <stdexcept>
-
-template <typename T>
-typename T::const_iterator easyfind(const T& container, int value) {
-    // try {
-        typename T::const_iterator it = container.begin();
-		for (; it != container.end(); ++it) {
-			if (*it == value)
-				return (it);
-		}
-        throw std::runtime_error("Value not found in container");
-    // } catch (const std::exception& e) {
-    //     std::cerr << "Error: " << e.what() << std::endl;
-    //     return container.end();
-    // }
-}
+# include <algorithm>
 
 template <typename T>
 typename T::iterator	easyfind(T &container, int value) {
-	// try {
-		typename T::iterator it = container.begin();
-		for (; it != container.end(); ++it) {
-			if (*it == value)
-				return (it);
-		}
-		throw std::runtime_error("Value not found in container");
-	// } catch (const std::exception& e) {
-	//     std::cerr << "Error: " << e.what() << std::endl;
-	//     return container.end();
-	// }
+	typename T::iterator i = std::find(container.begin(), container.end(), value);
+	if (i != container.end())
+		return i;
+	throw std::runtime_error("Value not found in container");
 }
 
 #endif
